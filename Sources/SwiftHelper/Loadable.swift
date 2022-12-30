b7dd93349bd95f830b82681a5fdd6e26754456b4
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-12-30 11:52:24 +0100

    Make generic loadable equatable

--- Sources/SwiftHelper/Loadable.swift
+++ Sources/SwiftHelper/Loadable.swift
@@ -7,32 +7,10 @@
 
 import Foundation
 
-public enum Loadable<T>: Equatable where T: Codable, T: Equatable {
+public enum Loadable<Item, Failure>: Equatable where Item: Codable, Item: Equatable, Failure: Error, Failure: Equatable {
     case none
     case loading
     case refreshing
-    case error(Error)
-    case loaded(T)
-
-    public static func == (lhs: Loadable<T>, rhs: Loadable<T>) -> Bool {
-        switch (lhs, rhs) {
-        case (.none, .none):
-            return true
-
-        case (.loading, .loading):
-            return true
-
-        case (.refreshing, .refreshing):
-            return true
-
-        case (let .error(lhsError), let .error(rhsError)):
-            return lhsError.localizedDescription == rhsError.localizedDescription
-
-        case (let .loaded(lhsLoaded), let .loaded(rhsLoaded)):
-            return lhsLoaded == rhsLoaded
-
-        default:
-            return false
-        }
-    }
+    case error(Failure)
+    case loaded(Item)
 }
